a34b47f731245d5fc7cf9a6954648f2f49ba779e
Montilla Kryštof <montilla.1str14@outlook.com>
2018-04-27 19:00:35 +0200

    Repo Init

---
+++ Controller/Controller.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Controller.ino
+ Created:	27.04.2018 17:43:59
+ Author:	monti
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	27.04.2018 17:43:35
+ Author:	monti
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
